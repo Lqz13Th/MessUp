20c0fd499ebce5ff509ab6e8d001ef06b2dbcb35
Grayman <Lqz13th@outlook.com>
2024-01-12 14:33:35 +0800

    branch test

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('grayman')
+    print_hi('gray_man')
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
